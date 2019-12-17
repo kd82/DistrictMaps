a3d9da3657512ff263f3b1bbe827dd338494ac28
Krishna Doddapaneni <krishna.doddapaneni@aristotle.com>
2019-12-17 11:27:32 -0500

    Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild } from '@angular/core';
+declare var google:any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('gmap',null) gmapElement : any;
+  map: google.maps.Map;
   title = 'my-maps-project';
+
+  ngOnInit(){
+    var mapProp = {
+      center: new google.maps.LatLng(38.887250, -77.003190-77.003190),
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
+  }
 }
